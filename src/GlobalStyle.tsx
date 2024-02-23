8f73dbfffa7f9b2e3c218a0a14acecb1cf4b67a7
Alessandro Gomes <alllessandrogomes@gmail.com>
2024-02-22 23:19:15 -0300

    fix: consertando novamente caminho de fonte local da Logo

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -5,7 +5,7 @@ export const GlobalStyle = createGlobalStyle`
 
     @font-face {
         font-family: 'Luckiest-Guy';
-        src: url("./public/fonts/Luckiest_Guy/LuckiestGuy-Regular.ttf");
+        src: url("/fonts/Luckiest_Guy/LuckiestGuy-Regular.ttf");
     }
 
     body {
