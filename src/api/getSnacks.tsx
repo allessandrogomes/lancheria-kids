16c97e3872ba01a1d83ae50917769b7c41f8f203
Alessandro Gomes <alllessandrogomes@gmail.com>
2024-02-24 22:13:43 -0300

    fix: os dados agora estão sendo buscados por meio de uma URL Raw do github

--- src/api/getSnacks.tsx
+++ src/api/getSnacks.tsx
@@ -1,7 +1,7 @@
 
 export async function getSnacks() {
     try {
-        const response = await fetch('/public/data/snacks.json')
+        const response = await fetch('https://raw.githubusercontent.com/allessandrogomes/lancheria-kids/main/public/data/snacks.json')
 
         if (!response.ok) {
             throw new Error('Não foi possível buscar os dados')
