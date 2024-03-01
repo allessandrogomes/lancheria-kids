76cd1e809300be9eb4c407adfc830d6d7e85f7dd
Alessandro Gomes <alllessandrogomes@gmail.com>
2024-03-01 18:09:28 -0300

    feat: Informações do formulário e do pedido são enviadas para o whatsapp(em teste)

--- src/components/Main/SnackBag/FormCustomerDetailsAndCompleteOrder/index.tsx
+++ src/components/Main/SnackBag/FormCustomerDetailsAndCompleteOrder/index.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components"
+import { useBag } from "../../../../context/BagContext";
 
 const Form = styled.form`
     display: flex;
@@ -31,25 +32,76 @@ const FormCustomerDetailsAndCompleteOrder = () => {
         referencePoint: ''
     });
 
+    const { bag } = useBag()
+
+    const [messageToWhatsapp, setMessageToWhatsapp] = useState('')
+
     const handleInputChange = (event: any) => {
         const { name, value } = event.target
         setFormData({ ...formData, [name]: value })
     };
 
-    const handleSubmit = (event: any) => {
+    const handleSubmitForm = (event: any) => {
         event.preventDefault()
-        console.log(formData)
+        setMessageToWhatsapp(() => {
+            const message = `
+                ${formData.name}
+                ${formData.city}
+                ${bag.totalAmountSnacks}
+                ${bag.totalPriceSnacks}
+            `
+            return message
+        })
     };
 
+    useEffect(() => {
+        if (messageToWhatsapp) {
+            window.open(`https://api.whatsapp.com/send?phone=5574999658200&text=${messageToWhatsapp}`, '_blank')
+        }
+    }, [messageToWhatsapp])
+
+    const formatTheCustomerName = (input: any) => {
+        const costumerName = removeNumbersTypedInInput(input)
+        setFormData({ ...formData, name: costumerName })
+    }
+
+    const phoneNumberFormat = (input: any) => {
+        let valueWithJustNumbers = removeCharactersTypedInInput(input)
+
+        let numberFormat = /^(\d{2})(\d{5})(\d{4})$/;
+        if (valueWithJustNumbers.length === 11) {
+            input.target.value = valueWithJustNumbers.replace(numberFormat, '($1) $2-$3');
+        } else {
+            input.target.value = valueWithJustNumbers
+        }
+
+        setFormData({ ...formData, phone: input.target.value })
+    }
+
+    const houseNumberFormat = (input: any) => {
+        const houseNumber = removeCharactersTypedInInput(input)
+        setFormData({ ...formData, number: houseNumber })
+    }
+
+    const removeCharactersTypedInInput = (input: any) => {
+        const resultWithJustNumbers = input.target.value.replace(/\D/g, '')
+        return resultWithJustNumbers
+    }
+
+    const removeNumbersTypedInInput = (input: any) => {
+        const resultWithJustLetters = input.target.value.replace(/[^a-zA-ZÀ-ÿ\s]/g, '')
+        return resultWithJustLetters
+    }
+
     return (
-        <Form onSubmit={handleSubmit}>
+        <Form onSubmit={handleSubmitForm}>
             <h2>Preencha seus dados para finalizar o pedido</h2>
 
             <label htmlFor="name">Nome Completo*</label>
-            <input type="text" id="name" name="name" value={formData.name} onChange={handleInputChange} required />
+            <input type="text" id="name" name="name" value={formData.name} onChange={handleInputChange} onKeyUp={(event) => formatTheCustomerName(event)} required />
 
             <label htmlFor="phone">Telefone*</label>
-            <input type="text" id="phone" name="phone" value={formData.phone} onChange={handleInputChange} required />
+            <input type="text" id="phone" name="phone" maxLength={15} value={formData.phone} onChange={handleInputChange} onKeyUp={(event) => phoneNumberFormat(event)} required />
 
             <label htmlFor="city">Cidade*</label>
             <select id="city" name="city" required value={formData.city} onChange={handleInputChange}>
@@ -64,13 +116,12 @@ const FormCustomerDetailsAndCompleteOrder = () => {
             <input type="text" id="street" name="street" value={formData.street} onChange={handleInputChange} required />
 
             <label htmlFor="number">Número*</label>
-            <input type="text" id="number" name="number" value={formData.number} onChange={handleInputChange} required />
+            <input type="text" id="number" name="number" value={formData.number} onChange={handleInputChange} onKeyUp={(event) => houseNumberFormat(event)} required />
 
             <label htmlFor="referencePoint">Ponto de referência</label>
-            <input type="text" id="referencePoint" name="referencePoint" value={formData.referencePoint} onChange={handleInputChange}/>
-            
-            <button type="submit" value="Enviar"><a href="https://api.whatsapp.com/send?phone=5574999658200&text=Ol%C3%A1%2C%20como%20vai%3F" target="_blank">Enviar mensagem pelo WhatsApp</a></button>
-            
+            <input type="text" id="referencePoint" name="referencePoint" value={formData.referencePoint} onChange={handleInputChange} />
+
+            <button type="submit" value="Enviar">Enviar pedido pelo WhatsApp</button>
 
         </Form>
     )
